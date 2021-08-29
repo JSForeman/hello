7975446cdf85e18315be4d143eae74ef2b25862c
Jack Foreman <jforeman5@huskers.unl.edu>
2021-08-29 21:58:15 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Jack Foreman
+ * Date: 2021/08/29
+ * Program that prints my name
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+   printf("Jack Foreman \n");
+
+   return 0;
+ }
